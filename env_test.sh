b555e309aa8ade28dba9a9850e01bafb8d78c9c8
Vladimir Levin <2969361+nestedsoftware@users.noreply.github.com>
2018-06-13 13:18:28 -0600

    minor change

--- env_test.sh
+++ env_test.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=~/dev/flask_sqlalchemy_starter
-export SQLALCHEMY_USERNAME=vlad
+export SQLALCHEMY_USERNAME=nestedsoftware
 export SQLALCHEMY_PASSWORD=simple
 export SQLALCHEMY_DBNAME=TestDB
 export SQLALCHEMY_URL="sqlite:///:memory:"
