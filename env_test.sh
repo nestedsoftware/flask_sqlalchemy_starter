3d4b07cd288a8dd532d36ab36587e1d22eb4d0e8
Vladimir Levin <2969361+nestedsoftware@users.noreply.github.com>
2018-06-13 13:26:15 -0600

    minor chagne

--- env_test.sh
+++ env_test.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=~/dev/flask_sqlalchemy_starter
-export SQLALCHEMY_USERNAME=nestedsoftware
-export SQLALCHEMY_PASSWORD=simple
-export SQLALCHEMY_DBNAME=TestDB
+export SQLALCHEMY_USERNAME=notapplicable
+export SQLALCHEMY_PASSWORD=notapplicable
+export SQLALCHEMY_DBNAME=notapplicable
 export SQLALCHEMY_URL="sqlite:///:memory:"
 
